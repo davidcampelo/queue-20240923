284bd3165cf5d969189e77ceda0008896b5b4f03
Jairo Duarte Ribeiro <jairoduarte123@gmail.com>
2024-09-19 22:34:15 +0000

    Add doxygen

--- src/queue.h
+++ src/queue.h
@@ -1,3 +1,7 @@
+/**
+ * @file queue.h
+ * @brief This file provides the declaration of a thread-safe queue class.
+ */
 #ifndef SRC_QUEUE_H_
 #define SRC_QUEUE_H_
 
@@ -7,22 +11,54 @@
 #include <stdexcept>
 #include <type_traits>
 
+/**
+ * @brief Concept to check if a type is a primitive type.
+ *
+ * @tparam T The type to check.
+ */
 template <typename T>
 concept Primitive = std::is_fundamental_v<T>;
 
+/**
+ * @brief A thread-safe queue implementation.
+ *
+ * @tparam T The type of the elements in the queue.
+ *
+ * @details This class provides a thread-safe queue implementation that can be
+ * used to communicate between threads. The queue has a fixed size and will
+ * overwrite the oldest element when it is full. The queue provides methods to
+ * push and pop elements, as well as a method to pop an element with a timeout.
+ * The queue is implemented using a circular buffer and uses a mutex and a
+ * condition variable to synchronize access to the buffer.
+ *
+ * @note This class is not copyable or movable.
+ */
 template <Primitive T>
 class Queue {
  public:
+  /**
+   * @brief Construct a new Queue object.
+   *
+   * @param size The size of the queue.
+   */
   Queue(int size) : size_{size}, head_{0}, tail_{0}, count_{0}, is_stopping_{false} {
     data_ = new T[size];
   }
 
+  /**
+   * @brief Destroy the Queue object.
+   */
   virtual ~Queue() {
     is_stopping_ = true;
     cv_.notify_all();
     delete[] data_;
   }
 
+  /**
+   * @brief Push an element to the queue.
+   *
+   * @param value The value to push to the queue.
+   */
   void Push(T value) {
     std::unique_lock<std::mutex> lock{mutex_};
     if (count_ == size_) {
@@ -33,6 +69,11 @@ class Queue {
     cv_.notify_one();
   }
 
+  /**
+   * @brief Pop an element from the queue.
+   *
+   * @return T The value popped from the queue.
+   */
   T Pop() {
     std::unique_lock<std::mutex> lock{mutex_};
     cv_.wait(lock, [this] { return count_ > 0 || is_stopping_; });
@@ -42,6 +83,14 @@ class Queue {
     return Dequeue();
   }
 
+  /**
+   * @brief Pop an element from the queue with a timeout.
+   *
+   * @param milliseconds The timeout in milliseconds.
+   * @return T The value popped from the queue.
+   *
+   * @throws std::runtime_error If the timeout is reached.
+   */
   T PopWithTimeout(int milliseconds) {
     std::unique_lock<std::mutex> lock{mutex_};
     if (!cv_.wait_for(lock, std::chrono::milliseconds(milliseconds),
@@ -54,17 +103,37 @@ class Queue {
     return Dequeue();
   }
 
+  /**
+   * @brief Get the number of elements in the queue.
+   *
+   * @return int The number of elements in the queue.
+   */
   int Count() const { return count_; }
 
+  /**
+   * @brief Get the size of the queue.
+   *
+   * @return int The size of the queue.
+   */
   int Size() const { return size_; }
 
  private:
+  /**
+   * @brief Enqueue an element to the queue.
+   *
+   * @param value The value to enqueue.
+   */
   inline void Enqueue(T value) {
     data_[tail_] = value;
     tail_ = (tail_ + 1) % size_;
     count_++;
   }
 
+  /**
+   * @brief Dequeue an element from the queue.
+   *
+   * @return T The value dequeued.
+   */
   inline T Dequeue() {
     T value = data_[head_];
     head_ = (head_ + 1) % size_;
@@ -72,14 +141,14 @@ class Queue {
     return value;
   }
 
-  T* data_;
-  const int size_;
-  int head_;
-  int tail_;
-  std::atomic<int> count_;
-  std::atomic<bool> is_stopping_;
-  mutable std::mutex mutex_;
-  std::condition_variable cv_;
+  T* data_;                       /*!< The data buffer. */
+  const int size_;                /*!< The size of the queue. */
+  int head_;                      /*!< The head of the queue. */
+  int tail_;                      /*!< The tail of the queue. */
+  std::atomic<int> count_;        /*!< The number of elements in the queue. */
+  std::atomic<bool> is_stopping_; /*!< Flag to indicate if the queue is stopping. */
+  mutable std::mutex mutex_;      /*!< The mutex to synchronize access to the queue. */
+  std::condition_variable cv_;    /*!< The condition variable to notify waiting threads. */
 };
 
 #endif  // SRC_QUEUE_H_

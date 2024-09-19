1a30ec2b2bbfbcc9725e70a03e16f88ac81d3c36
Jairo Duarte Ribeiro <jairoduarte123@gmail.com>
2024-09-19 13:35:01 +0000

    Configure debugger

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <string>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::string name{"Jairo"};
+    std::cout << "Hello, " << name << "!" << std::endl;
     return 0;
 }
